dbe82d6629f4882988c65e1c426d1cc2a51000b5
Arup paul <aruppaulctg@gmail.com>
2021-06-01 17:08:02 +0600

    done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import Navbar from "./Components/Navbar";
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import Home from './Components/pages/HomePage/Home'
 import Footer from "./Components/pages/Footer/Footer";
-
+import Services from './Components/pages/Services/Services'
+import Products from "./Components/pages/Products/Products";
+import Signup from "./Components/pages/SignUp/Signup";
 function App() {
   return (
     <Router>
         <Navbar />
         <Switch>
             <Route path='/' exact component={ Home } />
+            <Route path='/services' exact component={ Services } />
+            <Route path='/products' exact component={ Products } />
+            <Route path='/sign-up' exact component={ Signup } />
         </Switch>
         <Footer />
     </Router>
